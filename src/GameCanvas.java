74fd648a2e9aabeb2aab903bd61db3fb92866d42
quinn3111993 <nphuongquynh311@gmail.com>
2018-12-18 17:19:52 +0700

    update

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -7,9 +7,16 @@ import java.io.IOException;
 
 public class GameCanvas extends JPanel {
     BufferedImage background;
+    BufferedImage player;
     public GameCanvas() {
         try {
             background = ImageIO.read(new File("assets/images/background/0.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            player = ImageIO.read(new File("assets/images/players/straight/0.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -18,5 +25,6 @@ public class GameCanvas extends JPanel {
     protected void paintComponent(Graphics g) {
         g.fillRect(0,0,800,600);
         g.drawImage(background,0,0, null);
+        g.drawImage(player,(background.getWidth() - player.getWidth())/2,500,null);
     }
 }
